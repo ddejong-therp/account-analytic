ddbc888725ef68110408c8bc13d9c8a76d71a9d0
Danny de Jong <ddejong@therp.nl>
2022-05-09 15:19:38 +0200

    [IMP] procurement_mto_analytic: Merge the purchase orders for lines with different analytic accounts.

--- procurement_mto_analytic/models/purchase_order_line.py
+++ procurement_mto_analytic/models/purchase_order_line.py
@@ -6,14 +6,38 @@ from odoo import api, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    def _find_candidate(self, product_id, product_qty, product_uom,
+            location_id, name, origin, company_id, values):
+        so_account_analytic = (
+            values["move_dest_ids"][0].sale_line_id.order_id.analytic_account_id
+            if values["move_dest_ids"] else None
+        )
+        lines = self.filtered(
+            lambda po_line: po_line.account_analytic_id == so_account_analytic
+        ) if so_account_analytic else self
+        return super(PurchaseOrderLine, lines)._find_candidate(
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            company_id,
+            values,
+        )
+
     @api.model
     def _prepare_purchase_order_line_from_procurement(
         self, product_id, product_qty, product_uom, company_id, values, po
     ):
-        res = super(
-            PurchaseOrderLine, self
-        )._prepare_purchase_order_line_from_procurement(
+        so_account_analytic = (
+            values["move_dest_ids"][0].sale_line_id.order_id.analytic_account_id
+            if values["move_dest_ids"]
+            else None
+        )
+        res = super()._prepare_purchase_order_line_from_procurement(
             product_id, product_qty, product_uom, company_id, values, po
         )
-        res["account_analytic_id"] = values.get("account_analytic_id", False)
+        if so_account_analytic:
+            res["account_analytic_id"] = so_account_analytic.id
         return res
